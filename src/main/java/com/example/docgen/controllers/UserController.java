6560984f8a8bf0e527c86877d14eb126710221a5
Juan Guilherme Silva Lemos <apoio@gotreeconsultoria.com.br>
2025-07-08 10:28:35 -0300

    feat(user-mapper): adiciona UserMapperDTO para conversão entre entidades e DTOs
    - Criado  com métodos estáticos para conversão entre  ↔  e  ↔
    - Atualizado o  para utilizar o mapper ao invés de construir DTOs manualmente
    - Centralização da lógica de conversão, promovendo melhor organização e reutilização de código

--- src/main/java/com/example/docgen/controllers/UserController.java
+++ src/main/java/com/example/docgen/controllers/UserController.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.docgen.dto.UserMapperDTO;
 import com.example.docgen.dto.UserRequestDTO;
 import com.example.docgen.dto.UserResponseDTO;
 import com.example.docgen.entities.User;
 import com.example.docgen.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -27,21 +31,21 @@ public class UserController {
 	@GetMapping
 	public ResponseEntity<List<UserResponseDTO>> findAll() {
 		List<User> users = userService.findAll();
-		List<UserResponseDTO> usersDTO = users.stream().map(UserResponseDTO::new).toList();
-
-		return ResponseEntity.ok(usersDTO);
+		return ResponseEntity.ok(UserMapperDTO.toDtoList(users));
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
-		return ResponseEntity.ok(new UserResponseDTO(user));
+		return ResponseEntity.ok(UserMapperDTO.toDto(user));
 
 	}
-	
-	
-	
 
-	
+	@PostMapping
+	public ResponseEntity<UserResponseDTO> insertUser(@RequestBody @Valid UserRequestDTO dto) {
+		User user = UserMapperDTO.toEntity(dto);
+		User createdUser = userService.insertUser(user);
+		return ResponseEntity.ok().body(UserMapperDTO.toDto(createdUser));
+	}
 
 }
